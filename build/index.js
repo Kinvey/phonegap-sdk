a4d06c685486697046ff956a7f6f37a3bfe1552d
Thomas Conner <thomas@kinvey.com>
2016-04-18 14:05:49 -0400

    Alias device and popup in build

--- build/index.js
+++ build/index.js
@@ -8,25 +8,11 @@ var _serialize = require('kinvey-javascript-sdk-core/build/rack/middleware/seria
 
 var _http = require('./http');
 
-var _device = require('kinvey-javascript-sdk-core/build/utils/device');
-
-var _device2 = require('./device');
-
-var _popup = require('kinvey-javascript-sdk-core/build/utils/popup');
-
-var _popup2 = require('./popup');
-
 var _push = require('./push');
 
 // Add Http middleware
 var networkRack = _rack.NetworkRack.sharedInstance();
 networkRack.useAfter(_serialize.SerializeMiddleware, new _http.HttpMiddleware());
-
-// Use Device Adapter
-_device.Device.use(new _device2.DeviceAdapter());
-
-// Use Popup Adapter
-_popup.Popup.use(new _popup2.PopupAdapter());
 
 // Add Push module
 _kinveyJavascriptSdkCore.Kinvey.Push = _push.Push;
--- build/popup.js
+++ build/popup.js
@@ -3,9 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.PopupAdapter = undefined;
+exports.Popup = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _events = require('events');
 
 var _utils = require('./utils');
 
@@ -17,19 +19,27 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 /**
  * @private
  */
 
-var PopupAdapter = exports.PopupAdapter = function () {
-  function PopupAdapter() {
-    _classCallCheck(this, PopupAdapter);
+var Popup = exports.Popup = function (_EventEmitter) {
+  _inherits(Popup, _EventEmitter);
+
+  function Popup() {
+    _classCallCheck(this, Popup);
+
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(Popup).apply(this, arguments));
   }
 
-  _createClass(PopupAdapter, [{
+  _createClass(Popup, [{
     key: 'open',
     value: function open() {
-      var _this = this;
+      var _this2 = this;
 
       this.eventListeners = {
         loadHandler: (0, _bind2.default)(this.loadHandler, this),
@@ -38,16 +48,16 @@ var PopupAdapter = exports.PopupAdapter = function () {
 
       var promise = new Promise(function (resolve, reject) {
         if ((0, _utils.isBrowser)()) {
-          _this.popup = global.open(_this.url, '_blank', 'toolbar=no,location=no');
+          _this2.popup = global.open(_this2.url, '_blank', 'toolbar=no,location=no');
 
-          if (_this.popup) {
-            _this.interval = setInterval(function () {
-              if (_this.popup.closed) {
-                _this.closeHandler();
+          if (_this2.popup) {
+            _this2.interval = setInterval(function () {
+              if (_this2.popup.closed) {
+                _this2.closeHandler();
               } else {
                 try {
-                  _this.loadHandler({
-                    url: _this.popup.location.href
+                  _this2.loadHandler({
+                    url: _this2.popup.location.href
                   });
                 } catch (e) {
                   // catch any errors due to cross domain issues
@@ -58,27 +68,27 @@ var PopupAdapter = exports.PopupAdapter = function () {
             return reject(new Error('The popup was blocked.'));
           }
         } else {
-          _this.popup = global.open(_this.url, '_blank', 'location=yes');
+          _this2.popup = global.open(_this2.url, '_blank', 'location=yes');
 
-          if (_this.popup) {
-            _this.popup.addEventListener('loadstart', _this.eventListeners.loadHandler);
-            _this.popup.addEventListener('exit', _this.eventListeners.closeHandler);
+          if (_this2.popup) {
+            _this2.popup.addEventListener('loadstart', _this2.eventListeners.loadHandler);
+            _this2.popup.addEventListener('exit', _this2.eventListeners.closeHandler);
           } else {
             return reject(new Error('The popup was blocked.'));
           }
         }
 
-        return resolve(_this);
+        return resolve(_this2);
       });
       return promise;
     }
   }, {
     key: 'close',
     value: function close() {
-      var _this2 = this;
+      var _this3 = this;
 
       var promise = new Promise(function (resolve) {
-        _this2.popup.close();
+        _this3.popup.close();
         resolve();
       });
       return promise;
@@ -103,5 +113,5 @@ var PopupAdapter = exports.PopupAdapter = function () {
     }
   }]);
 
-  return PopupAdapter;
-}();
+  return Popup;
+}(_events.EventEmitter);
