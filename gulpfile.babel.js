f8a1c8fdd8ef1a90307ffcf9d9d18a54184ea5bb
Thomas Conner <thomas@kinvey.com>
2016-06-09 15:38:59 -0400

    Expose Kinvey var

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -59,7 +59,9 @@ gulp.task('bundle', ['build'], () => {
         './index.js'
       ],
       output: {
-        filename: `kinvey-phonegap-sdk-${pkg.version}.js`
+        filename: `kinvey-phonegap-sdk-${pkg.version}.js`,
+        libraryTarget: 'var',
+        library: 'Kinvey'
       },
       module: {
         loaders: [
