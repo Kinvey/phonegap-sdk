855368fdd8ce7b3bd308613670220384a0e830b8
Thomas Conner <thomas@kinvey.com>
2017-08-29 11:54:41 -0400

    Use custom cache middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import { NetworkRack } from 'kinvey-js-sdk/dist/export';
+import { CacheRack, NetworkRack } from 'kinvey-js-sdk/dist/export';
 import { MobileIdentityConnect } from 'kinvey-js-sdk/dist/identity';
 import Kinvey from 'kinvey-html5-sdk';
-import { HttpMiddleware } from './middleware';
+import { CacheMiddleware, HttpMiddleware } from './middleware';
 import Popup from './popup';
 import Push from './push';
 
 // Setup racks
+CacheRack.useCacheMiddleware(new CacheMiddleware());
 NetworkRack.useHttpMiddleware(new HttpMiddleware());
 
 // Setup popup
